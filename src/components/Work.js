ede2a1de4eab774b064e98c0a9b713ca5fbd3304
Nicole WItherall <nicwitherall@gmail.com>
2021-06-27 16:16:30 -0400

    style work component

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,4 +1,3 @@
-import { pink } from 'color-name';
 import React from 'react';
 import closeUp from "../images/close-up.jpg";
 import fallFlowers from "../images/fall-flower.jpg";
@@ -11,7 +10,8 @@ const Work = () => {
     return (
         <div className="Work">
             <div className="work-text">
-
+                <p>Whether it's roses, wildflowers, or everything in between: we love to create custom bouquets with some real creative flair. Flowershop was founded with the principle of collaboration in mind, and we love to collaborate with our customers to create their perfect floral arrangements for any occasion.</p>
+                <p>Want to get even more hands on? We offer both one-on-one and small group classes. Contact us to get more details or set up your next class.</p>
             </div>
             <div className="work-gallery">
                 <img src={closeUp} alt="a close up photo of pink flowers in a vase" />
