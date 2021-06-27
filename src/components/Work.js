06f4bafa6d783925d3e95e02e401ac3477833995
Nicole WItherall <nicwitherall@gmail.com>
2021-06-27 15:37:39 -0400

    import photos and set up structure of work component

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,9 +1,26 @@
+import { pink } from 'color-name';
 import React from 'react';
+import closeUp from "../images/close-up.jpg";
+import fallFlowers from "../images/fall-flower.jpg";
+import woodFlowers from "../images/flowers-on-wood.jpg";
+import peachAndWhiteFlowers from "../images/peach-and-white.jpg";
+import peachFlowers from "../images/peachy.jpg";
+import redFlowers from "../images/redflowers-dark.jpg";
 
 const Work = () => {
     return (
         <div className="Work">
-            <h1>work</h1>
+            <div className="work-text">
+
+            </div>
+            <div className="work-gallery">
+                <img src={closeUp} alt="a close up photo of pink flowers in a vase" />
+                <img src={fallFlowers} alt="a bouquet of flowers on a white background" />
+                <img src={woodFlowers} alt="a bouquet of flowers on a wooden table" />
+                <img src={peachAndWhiteFlowers} alt="peach and white hued flowers" />
+                <img src={peachFlowers} alt="a photo of peach color flowers" />
+                <img src={redFlowers} alt="peach and red color flowers on a dark background" />
+            </div>
         </div>
     )
 }
